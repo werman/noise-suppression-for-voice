ecdd69a0a7fa339654943f3f7f55fd678dce1035
Danylo Piliaiev <dpiliaiev@igalia.com>
2026-05-29 19:44:51 +0200

    build: Adjust project for JUCE 7.0.12

--- src/juce_plugin/RnNoiseAudioProcessor.h
+++ src/juce_plugin/RnNoiseAudioProcessor.h
@@ -16,6 +16,7 @@ public:
 
     bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
 
+    using juce::AudioProcessor::processBlock;
     void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
     juce::AudioProcessorEditor *createEditor() override;
