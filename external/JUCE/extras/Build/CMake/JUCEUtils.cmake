5db9b883737ffa1cf4c6393ef3f1b910d1ac6270
Danylo Piliaiev <dpiliaiev@igalia.com>
2026-05-29 19:43:26 +0200

    JUCE: Add LV2_COPY_DIR property

--- external/JUCE/extras/Build/CMake/JUCEUtils.cmake
+++ external/JUCE/extras/Build/CMake/JUCEUtils.cmake
@@ -1889,6 +1889,7 @@ function(_juce_initialise_target target)
         ARA_FACTORY_ID
         ARA_DOCUMENT_ARCHIVE_ID
 
+        LV2_COPY_DIR
         VST_COPY_DIR
         VST3_COPY_DIR
         AAX_COPY_DIR
@@ -1941,6 +1942,7 @@ function(_juce_initialise_target target)
         COMPANY_WEBSITE
         COMPANY_EMAIL
         COMPANY_COPYRIGHT
+        LV2_COPY_DIR
         VST_COPY_DIR
         VST3_COPY_DIR
         AU_COPY_DIR
